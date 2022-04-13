e8154b8e22e47a544c3440041613c3b8d877a416
Jakub Flaska <jflaska@baosystems.com>
2022-04-13 15:21:49 -0400

    Automatic lib publish

---
+++ easytest/modules/select.utils.ts
@@ -0,0 +1,36 @@
+import {fireEvent, getByText, screen, within} from "@testing-library/react";
+
+
+/**
+ * Select `value` in select element found by `id`
+ *
+ * @example
+ * ```javascript
+ * select('pizzaDough','newYorkSyle')
+ * ```
+ * @category Select
+ * */
+export function select(id:string, value:string){
+    fireEvent.mouseDown(screen.getByTestId(id).childNodes[0]);
+    let modal = screen.getByRole('presentation');
+    fireEvent.click(within(modal).getByText(value));
+    checkSelectValue(id, value);
+}
+
+export function selectById(id:string, value:string){
+    fireEvent.mouseDown(screen.getByTestId(id).childNodes[0]);
+    fireEvent.click(screen.getByTestId(value));
+}
+
+/**
+ * Check that select element found by `id` has value `value`
+ *
+ * @example
+ * ```javascript
+ * checkSelectValue('pizzaDough','newYorkSyle')
+ * ```
+ * @category Select
+ * */
+export function checkSelectValue(id:string, value:string){
+    expect(screen.getByTestId(id).textContent).toMatch(new RegExp(value));
+}
