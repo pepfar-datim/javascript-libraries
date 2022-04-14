dab7b4dc4b0384739e71593e056899bedfbe0c9c
Jakub Flaska <jflaska@baosystems.com>
2022-04-14 16:29:44 -0400

    Automatic lib publish

--- dep-copy/lib/services/removePeerDependencies.service.ts
+++ dep-copy/lib/services/removePeerDependencies.service.ts
@@ -4,6 +4,7 @@ import {getLocalPath} from "./path.service";
 import {splitPackageName} from "./getPackageName.service";
 
 export function removePeerDependencies(packageJson:PackageJson){
+    if (!packageJson.peerDependencies) return console.log(`No peer dependencies found`)
     let peerDependencies:string[] = Object.keys(packageJson.peerDependencies);
     peerDependencies.forEach((depName:string)=>{
         let packagePath = getLocalPath(splitPackageName(packageJson.name))+'/node_modules/'+depName;
