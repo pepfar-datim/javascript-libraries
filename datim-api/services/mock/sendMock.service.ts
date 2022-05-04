c898afb90493e8a7a4b08b8163dabd8560d98b6e
Jakub Flaska <jflaska@baosystems.com>
2022-05-04 14:19:46 -0400

    send mock and dep copy

--- datim-api/services/mock/sendMock.service.ts
+++ datim-api/services/mock/sendMock.service.ts
@@ -7,7 +7,7 @@ type SendMockRecord = {
     resolve:(payload:any)=>void
 }
 
-export function registerSendMock(url:string, response:any){
+export function registerSendMock(url:string, response:any):Promise<any>{
     return new Promise((resolve)=>{
         sendMockService.registerMockedResponse(url,{response,resolve});
     });
@@ -17,7 +17,7 @@ export function mockSendingData(url:string,payload:any):Promise<ApiResponse>{
     if (!isSendMocked(url)) throw new Error(`POST/PUT mock not in place`);
     let {resolve, response}:SendMockRecord = sendMockService.getMockedResponse(url);
     resolve(payload);
-    return Promise.resolve(response);
+    return Promise.resolve({responseBody:response,success:true,rawResponse:null});
 }
 
 export function isSendMocked(url:string):boolean{
