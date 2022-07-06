f4cb6e714e3444374fe95227c3f7ef11b8dff89e
Jakub Flaska <jflaska@baosystems.com>
2022-07-06 12:12:26 -0400

    Automatic lib publish

--- datim-api/services/mock/sendMock.service.ts
+++ datim-api/services/mock/sendMock.service.ts
@@ -4,20 +4,21 @@ import {ApiResponse} from "../../types/http.types";
 let sendMockService = new MockService();
 type SendMockRecord = {
     response:any,
-    resolve:(payload:any)=>void
+    resolve:(payload:any)=>void,
+    success
 }
 
-export function registerSendMock(url:string, response:any):Promise<any>{
+export function registerSendMock(url:string, response:any, success:boolean=true):Promise<any>{
     return new Promise((resolve)=>{
-        sendMockService.registerMockedResponse(url,{response,resolve});
+        sendMockService.registerMockedResponse(url,{response,resolve,success});
     });
 }
 
 export function mockSendingData(url:string,payload:any):Promise<ApiResponse>{
     if (!isSendMocked(url)) throw new Error(`POST/PUT mock not in place`);
-    let {resolve, response}:SendMockRecord = sendMockService.getMockedResponse(url);
+    let {resolve, response,success}:SendMockRecord = sendMockService.getMockedResponse(url);
     resolve(payload);
-    return Promise.resolve({responseBody:response,success:true,rawResponse:null});
+    return Promise.resolve({responseBody:response,success,rawResponse:null});
 }
 
 export function isSendMocked(url:string):boolean{
