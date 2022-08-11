06cb34c86e34318444b99588dd0eda6554144a05
Jakub Flaska <jflaska@baosystems.com>
2022-08-11 15:49:56 -0400

    Automatic lib publish

--- dhis2-loader-2/src/bin/insertUser.bin.ts
+++ dhis2-loader-2/src/bin/insertUser.bin.ts
@@ -1,7 +1,12 @@
 import {Dhis2User} from "../types/user.type";
 import {ServerConfig} from "../types/server.type";
-import {postJson} from "../modules/http/services/sendJson.service";
+import {postJson, putJson} from "../modules/http/services/sendJson.service";
 
-export function insertUser(user:Dhis2User, server:ServerConfig):Promise<any>{
-    return postJson('/users', user, server).catch(e=>console.log(e));
+export async function insertUser(user:Dhis2User, server:ServerConfig):Promise<any>{
+    try {
+        await postJson('/users', user, server)
+    } catch(e){
+        console.log(e)
+        return postJson('/users', user, server)
+    }
 }
