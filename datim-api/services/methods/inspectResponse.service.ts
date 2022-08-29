0d0071de83b65874a2ba2bd6da74760816c712e6
Jakub Flaska <jflaska@baosystems.com>
2022-08-29 13:34:18 -0400

    Automatic lib publish

--- datim-api/services/methods/inspectResponse.service.ts
+++ datim-api/services/methods/inspectResponse.service.ts
@@ -19,15 +19,13 @@ function fail(errorType:ErrorType, serverResponse:any):Error{
 }
 
 export async function inspectResponse(rawResponse:any):Promise<ApiResponse>{
-    if (!rawResponse.ok) throw fail(ErrorType.httpError,rawResponse);
-    if (rawResponse.redirected&&rawResponse.url.includes('login')) throw fail(ErrorType.silentRedirect,rawResponse)
-    if (rawResponse.status===204&&!rawResponse.redirected) return Promise.resolve({success:true, rawResponse});
-    let responseBody:any;
+    let responseBody = rawResponse;
     try {
         responseBody = JSON.parse(await rawResponse.text() as any);
     } catch (e){
-        return {success:true, rawResponse};
     }
+    if (!rawResponse.ok) throw fail(ErrorType.httpError,responseBody);
+    if (rawResponse.redirected&&rawResponse.url.includes('login')) throw fail(ErrorType.silentRedirect,responseBody)
     if (responseBody.status==='ERROR') throw fail(ErrorType.alreadyExists,responseBody)
     if (responseBody.status==='WARNING') throw fail(ErrorType.ignored,responseBody)
     return {success:true, responseBody, rawResponse};
