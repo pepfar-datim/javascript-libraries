810599066bb8a4be217084637efa1073f0965d3d
Jakub Flaska <jflaska@baosystems.com>
2024-02-08 15:33:30 -0500

    fix

--- local-import/import.ts
+++ local-import/import.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
+// Imports
+
 import cpy from 'cpy'
 import {readFileSync} from "node:fs";
 
-const modulePath = process.argv[2] || process.argv[1]
-
-if (!modulePath.includes('/')) throw Error(`Cannot recognize module path`)
+// Types
 
 type Map = {
     [key:string]:string
@@ -17,13 +17,31 @@ type PackageJson = {
     peerDependecies: Map
 }
 
+// Input
+
+const modulePath = process.argv[2] || process.argv[1]
+if (!modulePath.includes('/')) throw Error(`Cannot recognize module path`)
+
+// package.json
+
+const defaultIgnore = ['node_modules/.']
+
 const packageJson:PackageJson = JSON.parse(readFileSync(`${modulePath}/package.json`).toString())
 const [namespace, name] = packageJson.name.split('/')
-const peerAndDevDependencies:string[] = [...Object.keys(packageJson.devDependencies||[]), ...Object.keys(packageJson.peerDependecies||[])]
+const parse = (map:Map)=>Object.keys(map||[])
+
+const ignoredDependencies:string[] = [].concat(
+    defaultIgnore,
+    parse(packageJson.devDependencies),
+    parse(packageJson.peerDependecies)
+)
+
+// Copy
 
 console.log(`Copying files from ${modulePath} to ./node_modules/${namespace}/${name}`)
+console.log(`The following dependencies will be skipped`, ignoredDependencies)
 
 cpy(`${modulePath}/**/*`,`./node_modules/${namespace}/${name}`,{
-    filter: file => !peerAndDevDependencies.some(dependencyName=>file.relativePath.includes(`node_modules/${dependencyName}/`))
+    filter: file => !ignoredDependencies.some(dependencyName=>file.relativePath.includes(`node_modules/${dependencyName}/`))
 })
 
