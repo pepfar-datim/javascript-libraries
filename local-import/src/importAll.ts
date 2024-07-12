085c276a891e8246bdb033c9dd89363522c6901d
Jakub <jflaska@baosystems.com>
2024-07-12 11:53:55 -0400

    fix

--- local-import/src/importAll.ts
+++ local-import/src/importAll.ts
@@ -5,5 +5,5 @@ import {fork} from "node:child_process";
 const {import:importList}:PackageJson = JSON.parse(readFileSync(`package.json`).toString())
 
 importList.forEach((module)=>{
-    fork('node_modules/.bin/import',[`../${module}`])
+    fork('node_modules/.bin/importone',[`../${module}`])
 })
